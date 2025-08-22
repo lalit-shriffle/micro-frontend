85d347b75c87ef1f60841c43834e012ffc6bf97c
lalitp-shriffle <lalitp@shriffle.com>
2025-08-22 12:20:15 +0530

    Implemented centralized routing

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
-import  { useEffect, useState } from 'react'
 import ProductsApp from "products/ProductApp"
 import CartApp from "cart/CartApp"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
       <h1>Host App</h1>
-       <ProductsApp />
-       <CartApp/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/products' element={<ProductsApp/>} />
+          <Route path="/cart" element={<CartApp />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
